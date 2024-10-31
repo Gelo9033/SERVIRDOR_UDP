aef8543135eb7a1b0569e5c253530c68a46e991e
Luis Guaman <ivoylucho1990@gmail.com>
2024-10-31 16:29:15 -0500

    bug en form

--- src/gelo/chat_1/Cliente.java
+++ src/gelo/chat_1/Cliente.java
@@ -1,4 +1,50 @@
 package gelo.chat_1;
 
-public class Cliente {
+import javax.swing.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Cliente implements Runnable {
+
+private int puerto;
+private String mensaje;
+    private JButton enviarButton;
+    private JPanel panel1;
+    private JTextArea textArea1;
+
+    public Cliente(int puerto, String mensaje) {
+    this.puerto = puerto;
+    this.mensaje = mensaje;
 }
+
+
+    @Override
+    public void run() {
+        final String Host = "127.0.0.1";//ip de maquina
+        //puerto de servidor
+        //final int PUERTO=5000;
+        //DataInputStream in;
+        DataOutputStream out;
+
+        try {
+            Socket socket = new Socket(Host, puerto);
+            //in=new DataInputStream(socket.getInputStream());
+            out=new DataOutputStream(socket.getOutputStream());
+            out.writeUTF(mensaje);
+            //String mensaje=in.readUTF();
+            //System.out.println(mensaje);
+            socket.close();
+
+
+            }catch (IOException e){
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, e);
+        }
+        }}
+
+
+
+
+
--- src/gelo/chat_1/Servidor.java
+++ src/gelo/chat_1/Servidor.java
@@ -5,28 +5,48 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Servidor {
-    public static void main(String[] args) {
+public class Servidor extends Observable implements Runnable {
+
+    private int puerto;
+    public Servidor(int puerto) {
+        this.puerto = puerto;
+    }
+
+
+
+
+
+
+    @Override
+    public  void run() {
         ServerSocket servidor = null;
         Socket socket = null;
         DataInputStream in;
         DataOutputStream out;
-        final int PUERTO=5000;
+        //puerto de nuestro servidor
+        //final int PUERTO=5000;
 
         try {
-            servidor=new ServerSocket(PUERTO);
+            //creamos el socket del servidor
+            servidor=new ServerSocket(puerto);
             System.out.println("Servidor conectado");
+            //siempre estara escuchando peticiones
             while(true){
+                //espero a que el cliente se conecte
                 socket=servidor.accept();//socket de cliente
                 System.out.println("cliente conectado");
                 in=new DataInputStream(socket.getInputStream());
-                out=new DataOutputStream(socket.getOutputStream());
+                //out=new DataOutputStream(socket.getOutputStream());
                 String mensaje=in.readUTF();
-                System.out.println(mensaje);
-                out.writeUTF("hola desde el servidor");
+                //System.out.println(mensaje);
+                this.setChanged();
+                this.notifyObservers(mensaje);
+                this.clearChanged();
+                //out.writeUTF("hola desde el servidor");
                 socket.close();
                 System.out.println("cliente desconectado");
             }
